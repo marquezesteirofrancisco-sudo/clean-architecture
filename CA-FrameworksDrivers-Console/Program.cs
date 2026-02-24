816dbecb04bf5454952f4e80041f294ffe3b4cf2
Francisco Marquez Esteiro <117361707+franciscomes@users.noreply.github.com>
2026-02-24 17:21:09 +0100

    Se crea un proyecto de tipo console en la capa FrameworksDrivers-Console donde se configura la orquestacion para presentar la informacion por consola utilizando las capas interiores

---
+++ CA-FrameworksDrivers-Console/Program.cs
@@ -0,0 +1,49 @@
+﻿using CA_ApplicationLayer;
+using CA_interfaceAdapterData;
+using CA_InterfaceAdapters_Presenters;
+using CA_InterfaceAdapters_Repository;
+using CL_EnterpriseLayer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+
+IConfiguration configuration = builder.Build();
+
+
+var container = new ServiceCollection()
+        .AddDbContext<AppDbContext>(options => options.UseSqlServer(
+            configuration.GetConnectionString("ConexionSQLServer")))
+        .AddScoped<IRepository<Beer>, BeerRepository>()
+        .AddScoped<GetBeerUseCase<Beer, BeerViewModel>>()
+        .AddScoped<GetBeerUseCase<Beer, BeerDetailViewModel>>()
+        .AddScoped<IPresenter<Beer, BeerViewModel>, BeerPresenter>()
+        .AddScoped<IPresenter<Beer, BeerDetailViewModel>, BeerDetailPresenter>()
+        .BuildServiceProvider();
+
+
+var getBeerUseCase = container.GetRequiredService<GetBeerUseCase<Beer, BeerViewModel>>();
+
+var beers = await getBeerUseCase.ExecuteAsync();
+
+foreach (var beer in beers)
+{
+    Console.WriteLine($"Id: {beer.Id}, Name: {beer.Name}, Type: {beer.Style} , Alcohol: {beer.Alcohol}");
+}
+
+
+
+var getBeerUseCaseDetail = container.GetRequiredService<GetBeerUseCase<Beer, BeerDetailViewModel>>();
+
+var beersDetails = await getBeerUseCaseDetail.ExecuteAsync();
+
+foreach (var beer in beersDetails)
+{
+    Console.WriteLine($"Id: {beer.Id}, Name: {beer.Name}, Type: {beer.Style} , Alcohol: {beer.Alcohol} , Color: {beer.Color} , Message: {beer.Message} ");
+}
